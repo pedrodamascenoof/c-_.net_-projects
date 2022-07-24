c0ac9b2ee7534d89700be28994bea14756e76d5f
Pedro Damasceno <104572156+pedrodamascenoof@users.noreply.github.com>
2022-07-24 11:35:39 -0300

    Add files via upload

---
+++ Senha.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite seu nome: ");
+string nome = Console.ReadLine();
+Console.WriteLine("\n\n\n");
+Console.WriteLine("Digite sua senha: ");
+string senha = Console.ReadLine();
+string senha2 = "3103001458";
+if (senha == senha2)
+{
+    Console.Write("Bem vindo ");
+    Console.WriteLine(nome);
+}
+else
+{
+    Console.Write("Senha incorreta.");
+}
